25e0f677ada7a5c2b2c4531beae9adc2459a6b36
anda16 <te16.david.andersson@umea.nti.se>
2018-11-16 12:21:42 +0100

    spelplan printad, tom

--- src/Board.java
+++ src/Board.java
@@ -1,11 +1,12 @@
 import java.util.List;
 
 public class Board {
-    private int width;
-    private int height;
+    private int width = 10;
+    private int height = 10;
     private List lights;
     private List fuel;
     private List robots;
+    private char[] board;
 
     public int getWidth() {
         return width;
@@ -47,7 +48,34 @@ public class Board {
         this.robots = robots;
     }
 
+    public char[] getBoard(){
+        return board;
+    }
+
+    public void board() {
+        board = new char[width*height];
+         for(int i = 0; i<(height*width); i++){
+            if (i % width == 0 || i % width + 1 == width || i / height == 0 || i / height + 1 == height) {
+                this.board[i] = 'X';
+            } else {
+                this.board[i] = ' ';
+            }
+        }
+    }
+
+    public Board(){
+        this.board();
+    }
+
     public static void main(String[] args) {
-        
+        Board b = new Board();
+        for(int i = 0; i<b.getHeight(); i++){
+            String out = "";
+            for(int k = 0; k<b.getWidth(); k++){
+                out = out + b.getBoard()[k+i*b.getWidth()] + " ";
+            }
+            System.out.println(out);
+        }
+
     }
 }
