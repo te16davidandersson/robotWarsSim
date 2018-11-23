b383d38ca5f385eb4b6115b24f2895f4b6e2b3e6
te16davidandersson <te16.david.andersson@umea.nti.se>
2018-11-24 00:30:20 +0100

    La till robotar, lampor och rensade upp koden lite mer

--- src/Simulation.java
+++ src/Simulation.java
@@ -1,7 +1,16 @@
 public class Simulation {
     public static void main(String[] args) {
         Board board = new Board();
-        board.createFuel((int)Math.floor(Math.random()*8) + 1,(int)(Math.floor(Math.random()*8))+ 1);
+        initBoard(board);
+        //TODO: While loop med wait time mellan t.ex. thread.sleep eller timer
         board.printBoard();
+    }
+
+    public static void initBoard(Board board) {
+        //TODO: fixa så att de inte kan skapas på samma x och y värden
+        board.createFuel((int)Math.floor(Math.random()*8) + 1,(int)(Math.floor(Math.random()*8))+ 1);
+        board.createSource((int)Math.floor(Math.random()*8) + 1,(int)(Math.floor(Math.random()*8))+ 1);
+        board.createRobotLight((int)Math.floor(Math.random()*8) + 1,(int)(Math.floor(Math.random()*8))+ 1);
+        board.createRobotDark((int)Math.floor(Math.random()*8) + 1,(int)(Math.floor(Math.random()*8))+ 1);
     }
 }
